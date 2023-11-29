b9479c25b57d869fc16beb86342f668751e469ad
DaddaAdam <daddaadam@gmail.com>
2023-11-29 17:42:10 +0100

    fixed pom.xml and added communication between .properties files and microservices

--- config-service/src/main/java/com/emsi/configservice/ConfigServiceApplication.java
+++ config-service/src/main/java/com/emsi/configservice/ConfigServiceApplication.java
@@ -2,10 +2,12 @@ package com.emsi.configservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer // pour activer le serveur de configuration
+@EnableDiscoveryClient // pour activer le service de découverte
 public class ConfigServiceApplication {
 
 	public static void main(String[] args) {
