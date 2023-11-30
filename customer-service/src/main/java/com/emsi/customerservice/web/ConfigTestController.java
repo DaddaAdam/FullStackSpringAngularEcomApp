d42e82c03f84478d752c025e8da80761728756d7
DaddaAdam <daddaadam@gmail.com>
2023-11-30 10:28:35 +0100

    added database configuration in config service and added repository rest resouce to CustomerRepository

--- customer-service/src/main/java/com/emsi/customerservice/web/ConfigTestController.java
+++ customer-service/src/main/java/com/emsi/customerservice/web/ConfigTestController.java
@@ -8,18 +8,18 @@ import java.util.Map;
 
 @RestController
 public class ConfigTestController {
-    @Value("${global.params.p1}")
-    private String p1;
-    @Value("${global.params.p2}" )
-    private String p2;
-    @Value("${customer.params.x}")
-    private String x;
-    @Value("${customer.params.y}")
-    private String y;
-
-    @GetMapping("/config")
-    public Map<String, String> getConfig() {
-        return Map.of("p1", p1, "p2", p2, "x", x, "y", y);
-    }
+//    @Value("${global.params.p1}")
+//    private String p1;
+//    @Value("${global.params.p2}" )
+//    private String p2;
+//    @Value("${customer.params.x}")
+//    private String x;
+//    @Value("${customer.params.y}")
+//    private String y;
+//
+//    @GetMapping("/config")
+//    public Map<String, String> getConfig() {
+//        return Map.of("p1", p1, "p2", p2, "x", x, "y", y);
+//    }
 
 }
