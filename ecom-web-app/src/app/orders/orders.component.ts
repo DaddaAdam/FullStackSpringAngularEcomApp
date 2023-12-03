3f3e87667321d485b670e83bb48d1ae52d24f183
DaddaAdam <daddaadam@gmail.com>
2023-12-03 18:40:28 +0100

    created order-component and OrderProjection

---
+++ ecom-web-app/src/app/orders/orders.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-orders',
+  standalone: true,
+  imports: [],
+  templateUrl: './orders.component.html',
+  styleUrl: './orders.component.css'
+})
+export class OrdersComponent implements OnInit{
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
