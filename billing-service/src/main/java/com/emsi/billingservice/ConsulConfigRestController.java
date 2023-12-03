220bd1e1dedf54e9e05fb8f24f2e1b30f06a2240
DaddaAdam <daddaadam@gmail.com>
2023-12-03 16:17:39 +0100

    added vault configuration to the billing service

--- billing-service/src/main/java/com/emsi/billingservice/ConsulConfigRestController.java
+++ billing-service/src/main/java/com/emsi/billingservice/ConsulConfigRestController.java
@@ -4,13 +4,17 @@ import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 @AllArgsConstructor
 public class ConsulConfigRestController {
     private final MyConsulConfig myConsulConfig;
+    private final MyVaultConfig myVaultConfig;
 
     @GetMapping(path = "/myConfig")
-    public MyConsulConfig myConfig() {
-        return myConsulConfig;
+    public Map<String, Object> myConfig(){
+        return Map.of("consulConfig", myConsulConfig,
+                "vaultConfig", myVaultConfig);
     }
 }
